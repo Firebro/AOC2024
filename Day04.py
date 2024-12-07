3e9ff0798fb3e9d8933e817c4a9c26e3723977ef
Firebro <temy5210@gmx.de>
2024-12-07 20:36:22 +0100

    Update Day04.py

--- Day04.py
+++ Day04.py
@@ -1,4 +1,29 @@
+import re
+
 File = open("InputDay04.txt")
 input = File.read()
 
-print(input)
+lines = input.split("\n")
+xmas = re.compile("XMAS")
+
+counter = 0
+
+def check_xmas (line) -> int:
+    hor_res_list = re.findall(xmas, line)
+    hor_bckw_list = re.findall(xmas, line[::-1])
+
+    return len(hor_res_list)+ len(hor_bckw_list)
+
+
+vert_lines = []
+for letter in lines[0]:
+    vert_lines.append("")
+for line in lines:
+    for letter in range(len(line)):
+        vert_lines[letter] += line[letter]
+
+print(len(lines[0]))
+
+#counter += check_xmas(line)
+
+print(counter)
